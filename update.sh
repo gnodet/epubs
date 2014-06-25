65beb1bbc086434af9b393bce2844a8782444013
Guillaume Nodet <gnodet@gmail.com>
2014-06-26 01:36:18 +0200

    Add reports Add aeterni patris

--- update.sh
+++ update.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
-
+rm -Rf archives
 rm -Rf library
 rm -Rf epub
+rm -Rf reports
+
 cp -R ../epubs/target/site/ .
+
+mkdir reports
+cd reports
+mv ../epub/*.html .
+REPORT=""
+for FILE in *-report.html;
+do
+	filename=`cat $FILE | sed -n 's/.*"filename" : "\(.*\)",/\1/p'`;
+	nFatal=`cat $FILE | sed -n 's/.*"nFatal" : \([^,]*\),/\1/p'`;
+	nError=`cat $FILE | sed -n 's/.*"nError" : \([^,]*\),/\1/p'`;
+	nWarning=`cat $FILE | sed -n 's/.*"nWarning" : \([^,]*\),/\1/p'`;
+	valid=$((nFatal + nError))
+	if [[ $valid -ne 0 ]]
+	then
+		status="Invalide";
+	else
+		status="Valide";
+	fi
+	REPORT="$REPORT<tr><td><a href=\"$FILE\">$filename</a></td><td>$status</td><td>$nFatal</td><td>$nError</td><td>$nWarning</td></tr>";
+done
+cat ../report.html | sed -n '/__REPORT__/,$!p' > report.html
+echo $REPORT >> report.html
+cat ../report.html | sed -n '1,/__REPORT__/!p' >> report.html
+cd ..
+
 mkdir library
 cd library
 for FILE in ../epub/*.epub ;
@@ -35,3 +62,7 @@ echo "]" >> epub_library.json
 rm readium-js-viewer/epub_content/epub_library.json
 mv epub_library.json readium-js-viewer/epub_content/
 
+mkdir archives
+cd epub
+zip ../archives/epubs.zip *.epub
+cd ..
