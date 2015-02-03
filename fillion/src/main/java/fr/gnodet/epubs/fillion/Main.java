b0785f1f71f76568411d5b071b9fa43bb2a68f92
Guillaume Nodet <gnodet@gmail.com>
2015-02-03 21:00:37 +0100

    Fix marc epub generation

--- fillion/src/main/java/fr/gnodet/epubs/fillion/Main.java
+++ fillion/src/main/java/fr/gnodet/epubs/fillion/Main.java
@@ -10,26 +10,20 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        String[] evangelists = new String[] {
-                "Marc" //"Matthieu", "Marc", "Luc", "Jean"
-        };
-
-        String title = "Évangile selon saint ";
+        String title = "Évangile selon saint Marc";
         String creator = "Louis-Claude FILLION";
 
-        for (String evang : evangelists) {
             byte[] coverPng = Cover.generateCoverPng(Math.random(),
                     title,
                     new Object[]{
                             new Cover.Text(creator, new Font(Font.SERIF, Font.PLAIN, 58), 1.0, 0.0),
                             new Cover.Break(),
-                            new Cover.Text(title + evang, new Font(Font.SERIF, Font.PLAIN, 118), 1.2, 0.25),
+                            new Cover.Text(title, new Font(Font.SERIF, Font.PLAIN, 118), 1.2, 0.25),
                             new Cover.Break(),
                     },
                     null);
-            System.out.println("Copying cover to: " + System.getProperty("basedir") + "/target/docbkx/epub3/" + evang.toLowerCase() + "/xml/");
-                    writeToFile(coverPng, System.getProperty("basedir") + "/target/docbkx/epub3/" + evang.toLowerCase() + "/xml/cover.png");
-        }
+            System.out.println("Copying cover to: " + System.getProperty("basedir") + "/target/docbkx/epub3/");
+            writeToFile(coverPng, System.getProperty("basedir") + "/target/docbkx/epub3/cover.png");
     }
 
 }
